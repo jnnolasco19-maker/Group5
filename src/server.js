f97298a0cb2d18b58aca8e00de34e84e27cffea9
jnnolasco19-maker <jnnolasco19@gmail.com>
2026-02-11 23:12:04 +0800

    Configured project structure and environment variables

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const http = require('http');
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.write("Market Management System Server Running");
   res.end();
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
